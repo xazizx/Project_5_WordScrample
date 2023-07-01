4b423de9e2d826212bcb9607af4b2874bd82621b
Aziz Baubaid <3zuccino@gmail.com>
2023-07-01 17:22:58 +0200

    Initial Commit

---
+++ Project_5_WordScrample/Project_5_WordScrampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_5_WordScrampleApp.swift
+//  Project_5_WordScrample
+//
+//  Created by Aziz Baubaid on 01.07.23.
+//
+
+import SwiftUI
+
+@main
+struct Project_5_WordScrampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
